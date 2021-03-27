b06db924c56d2e8b18fb467899b46087de8e5878
Felipe <felipec.barroso97@gmail.com>
2021-03-27 16:57:25 -0300

    Aplicação trasnferencia

---
+++ BankTransfer/BankTransfer/Classes/Conta.cs
@@ -0,0 +1,58 @@
+﻿using BankTransfer.Enums;
+using System;
+
+namespace BankTransfer.Classes
+{
+    public class Conta
+    {
+        public Conta(string nome, double saldo, double credito, TipoConta tipoConta)
+        {
+            Nome = nome;
+            Saldo = saldo;
+            Credito = credito;
+            TipoConta = tipoConta;
+        }
+
+        private string Nome { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private TipoConta TipoConta { get; set; }
+
+
+        public bool Sacar(double valorSaque)
+        {
+            if (Saldo - valorSaque < (Credito * -1))
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+            Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta é de: ", Saldo);
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            Saldo = +valorDeposito;
+            Console.WriteLine("Saldo atuel da conta é de: ", Saldo);
+        }
+
+        public void Transferencia (double valorTransferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + TipoConta + " | ";
+            retorno += "Nome " + Nome + " | ";
+            retorno += "Saldo " + Saldo + " | ";
+            retorno += "Crédito " + Credito;
+            return retorno;
+        }
+    }
+}
---
+++ BankTransfer/BankTransfer/Enums/TipoConta.cs
@@ -0,0 +1,8 @@
+﻿namespace BankTransfer.Enums
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica =2
+    }
+}
